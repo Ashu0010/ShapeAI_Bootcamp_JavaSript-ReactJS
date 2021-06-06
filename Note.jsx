cf0329899127c487a601dff8d1f836678a2e7f94
Ashu0010 <85412509+Ashu0010@users.noreply.github.com>
2021-06-06 13:40:06 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div>
+      <div className="note">
+        <h1>JavaScript & ReactJS Bootcamp</h1>
+        <p>
+          This was an amazing 7 days Bootcamp in which we cover the basic about
+          the particular things related to the course.
+        </p>
+      </div>
+      <div className="note">
+        <h1>Python & Machine Learning Bootcamp</h1>
+        <p>
+          {" "}
+          This was an amazing 7 days Bootcamp in which we cover the basic about
+          the particular things related to the course.
+        </p>
+      </div>
+      <div className="note">
+        <h1>Web Dev with JavaScript & ReactJS Bootcamp</h1>
+        <p>
+          {" "}
+          This was an amazing 7 days Bootcamp in which we cover the basic about
+          the particular things related to the course.
+        </p>
+      </div>
+      <div className="note">
+        <h1>Web Devolpment Bootcamp</h1>
+        <p>
+          {" "}
+          This was an amazing 7 days Bootcamp in which we cover the basic about
+          the particular things related to the course.
+        </p>
+      </div>
+    </div>
+  );
+}
+export default Note;
